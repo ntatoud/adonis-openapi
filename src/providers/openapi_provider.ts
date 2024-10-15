d93234cd3343b8aae7e2656e9eb1d3aa35bfd2d5
Noe TATOUD <noe.tatoud@gmail.com>
2024-10-15 19:01:06 +0200

    feat(provider): add OpenapiServiceProvider and export it from package

---
+++ src/providers/openapi_provider.ts
@@ -0,0 +1,59 @@
+import type { ApplicationService } from '@adonisjs/core/types';
+import type { ResourceActionNames } from '@adonisjs/http-server/types';
+import type { ResourceMetadata } from '#extensions/adonis/routes/route_resource';
+import type { OpenApiMeta } from '../types/index.js';
+
+/**
+ * Extending AdonisJS types
+ */
+declare module '@adonisjs/core/http' {
+	export interface Router {
+		/**
+		 * Define the routes for API Documentation
+		 * - Documentation will be under `docsPath`
+		 * - Swagger file will be under `/swagger` (for now)
+		 */
+		openapi(docsPath: string): Route;
+	}
+
+	export interface Route {
+		/**
+		 * Define the openapi metadata associated to a route
+		 */
+		openapi(meta: OpenApiMeta): this;
+	}
+
+	export interface RouteResource<ActionNames extends ResourceActionNames = ResourceActionNames> {
+		/**
+		 * Define the openapi metadata associated to actions of a route resource
+		 */
+		openapi(resourceMetadata: ResourceMetadata): this;
+	}
+}
+/**
+ * OpenAPI service provider
+ */
+export default class OpenapiServiceProvider {
+	constructor(protected app: ApplicationService) {}
+
+	/**
+	 * Register Route openapi macro
+	 */
+	protected async registerRoutesOpenapi() {
+		const { defineRoutesOpenapi } = await import('../extensions/adonis/index.js');
+
+		await defineRoutesOpenapi();
+	}
+
+	/**
+	 * Invoked by AdonisJS to register container bindings
+	 */
+	register() {}
+
+	/**
+	 * Invoked by AdonisJS to extend the framework or pre-configure objects
+	 */
+	async boot() {
+		await this.registerRoutesOpenapi();
+	}
+}
